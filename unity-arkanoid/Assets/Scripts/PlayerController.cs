036ac5ec1c602c5d3cfa0d7ad0cab6d3668c8b1c
brian-comes-digipen <brian.comes@digipen.edu>
2020-01-30 17:22:11 -0800

    Working on prototype - Created prefabs for Paddle/Player, is functioning - Created prefab for the ball, not functioning (script has no content) - Renamed "Title.unity" to be "BrianTesting.unity" because I started making game stuff in the title scene - Created placeholder/default sprites (Default Unity sprite stuff)

--- unity-arkanoid/Assets/Scripts/PlayerController.cs
+++ unity-arkanoid/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float leftXBoundary = -96.5f;
+    public float rightXBoundary = 47.5f;
+    public float speedInPixelsPerFrame = 2;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x != -104.5)
+        {
+            transform.position -= (Vector3)new Vector2(speedInPixelsPerFrame, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow) && transform.position.x != 39.5)
+        {
+            transform.position += (Vector3)new Vector2(speedInPixelsPerFrame, 0);
+        }
+
+        if (transform.position.x > 47.5f)
+        {
+            transform.position = (Vector3)new Vector2(47.5f, transform.position.y);
+        }
+        if (transform.position.x < -96.5f)
+        {
+            transform.position = (Vector3)new Vector2(-96.5f, transform.position.y);
+        }
     }
 }
