afd9927cd8cf1b119625061af8b6c85d03a842f6
brian-comes-digipen <brian.comes@digipen.edu>
2020-01-31 15:54:07 -0800

    added arkanoid nes rom + emulator for reference gameplay (no more using a crappy web-based emulator)

--- unity-arkanoid/Assets/Scripts/GameManager.cs
+++ unity-arkanoid/Assets/Scripts/GameManager.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static int Level = 1;
+    public static int Lives = 3;
+    public static int Score = 0;
+    public static GameState gameState;
+    public enum GameState { Title, Game, Dead, Scores };
+    private static GameManager _instance;
+    public static GameManager instance
+    {
+        get
+        {
+            //if instance isn't found try to find it
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<GameManager>();
+                DontDestroyOnLoad(_instance);
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        //set the singleton _instance
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else if (_instance != this)
+        {
+            Destroy(this);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
