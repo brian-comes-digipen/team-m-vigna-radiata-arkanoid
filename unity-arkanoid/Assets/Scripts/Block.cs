b9cd16b6e5a420739759d5d6723e41f405540c85
brian-comes-digipen <brian.comes@digipen.edu>
2020-01-30 18:02:12 -0800

    Created very basic code for block breaking, need to add point functionality

--- unity-arkanoid/Assets/Scripts/Block.cs
+++ unity-arkanoid/Assets/Scripts/Block.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public int hitsToBreak = 1;
+    private int hitsRemaining;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hitsRemaining = hitsToBreak;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name.Contains("Ball"))
+        {
+            hitsRemaining--;
+            if (hitsRemaining <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
