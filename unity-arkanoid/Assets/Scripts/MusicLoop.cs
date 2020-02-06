d2ab10392578e659772049ebd32d3f92ff5b6145
Kyle-2 <48459007+Kyle-2@users.noreply.github.com>
2020-02-05 18:01:39 -0800

    added Loop

---
+++ unity-arkanoid/Assets/Scripts/MusicLoop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicLoop : MonoBehaviour
+{
+    public bool Started = false;
+
+    private RandomContainer randomC;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomC = GameObject.FindObjectOfType(typeof(RandomContainer)) as RandomContainer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Started)
+        {
+            if(randomC != null)
+            {
+
+            }
+        }
+    }
+}
