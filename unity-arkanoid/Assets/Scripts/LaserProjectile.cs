f39b939d66a14f99438d92aba400dab3d573788f
brian-comes-digipen <brian.comes@digipen.edu>
2020-02-06 16:17:27 -0800

    Fixed brick collision Made blocks take damage from any object whose name contains "laser" (meaning its a laserprojectile) Working on a laserprojectile prefab

---
+++ unity-arkanoid/Assets/Scripts/LaserProjectile.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserProjectile : MonoBehaviour
+{
+    public float speed = 200;
+    Rigidbody2D rb2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb2D.velocity = new Vector2(0, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<Block>() != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
